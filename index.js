b635c52496cd930b45640a9d6f7715b737e68da4
yutangtai <dajiu0224@gmail.com>
2021-05-30 22:44:56 +0800

    完成啦!!!!!
    調整釋字查詢的規則：E + 關鍵字 / 直接輸入數字

--- index.js
+++ index.js
@@ -39,8 +39,6 @@ __proto__: Array(0)
 // 選單四大選項狀態
 let expoOpen = false;
 let lawOpen = false;
-let newsOpen = false;
-let ruleOpen = false;
 
 // 法條查詢-四大法典狀態
 let xianfaOpen = false;
@@ -49,6 +47,8 @@ let shinfaOpen = false;
 let shinzenfaOpen = false;
 // 釋字查詢--------------------------------------------------->
 const expoMode = async(event) =>{
+  let userTypeStr = event.message.text;
+  let userTypeNumStr = exponNum(event);
   let page;
   let nowPage = 1;
   let totalPageNum;
@@ -56,54 +56,16 @@ const expoMode = async(event) =>{
   let titles = [];
   let titleLinks = [];
   let replyStr = '';
-  // 輸入關鍵字-----------------------> 
-  await axios
-  .get(`https://cons.judicial.gov.tw/jcc/zh-tw/jep03?interYear=&interNo=&interKeyword=${encodeURI(event.message.text)}&startDate=&endDate=&submit=`)
-  .then(function (response) {
-    const $ = cheerio.load(response.data)
-    page = $('.pagination .PagedList-pageCountAndLocation').find('a').text()
-    $('body > div.container.blocky_container.footer_fix > div.container.blocky_container.footer_fix > div > table > tbody > tr > td:nth-child(2)').each(
-      function (index, element) {
-        titles.push($(element).text().trim())
-        titleLinks.push($(element).find('a').attr('href'))
-      }
-      )
-    })
-    // 搜尋結果不止一頁
-    if (page !== '') {
-      console.log(page)
-      totalPageNum = parseInt(/共(\d+)頁/g.exec(page)[1])
-      while (nowPage <= totalPageNum) {
-        await axios
-        .get(`https://cons.judicial.gov.tw/jcc/zh-tw/jep03?page=${nowPage}&interKeyword=${encodeURI(event.message.text)}`)
-        .then(function (response) {
-          const $ = cheerio.load(response.data)
-          console.log(nowPage)
-          $(
-            'body > div.container.blocky_container.footer_fix > div.container.blocky_container.footer_fix > div > table > tbody > tr > td:nth-child(2)'
-            ).each(function (index, element) {
-              titles.push($(element).text().trim())
-              titleLinks.push($(element).find('a').attr('href'))
-            })
-            nowPage++
-        })
-      }
-    }
-    //整裡字串 
-    for (let i = 0; i < titles.length; i++) {
-      replyStr += `⚆ ${titles[i]}\n`
-    }
-    // 輸入 'Exxx' 字串---------------------------->
-    let expnoResponse = {};
-    let finalNum = exponNum(event);
+    // 直接輸入數字---------------------------------->
+    let expnoResponse = {};    
     let exponBubble;
-    if (event.message.text === `E${finalNum}`) {
-      await axios.get(`https://cons.judicial.gov.tw/jcc/zh-tw/jep03/show?expno=${finalNum}`).then(function (response) {
+    if(!isNaN(parseInt(userTypeStr))){
+      await axios.get(`https://cons.judicial.gov.tw/jcc/zh-tw/jep03/show?expno=${userTypeStr}`).then(function (response) {
         const $ = cheerio.load(response.data);
         expnoResponse.interpretation = $('#preExpTitle').text()
         expnoResponse.argument = `　　${$('#secEleven > div.content.pure_text > pre').text()}`
         expnoResponse.critique = $('#secOne > div.content.pure_text > ul > li > pre').text()
-        expnoResponse.link = `https://cons.judicial.gov.tw/jcc/zh-tw/jep03/show?expno=${finalNum}`
+        expnoResponse.link = `https://cons.judicial.gov.tw/jcc/zh-tw/jep03/show?expno=${userTypeStr}`
         let inteArr;
         let newInteStr = '';
       if (expnoResponse.interpretation.split('').length > 7) {
@@ -130,7 +92,7 @@ const expoMode = async(event) =>{
             contents: [
               {
                 type: 'text',
-                text: `🚩 釋字第 ${finalNum} 號`,
+                text: `🚩 釋字第 ${userTypeStr} 號`,
                 weight: 'bold',
                 size: 'xl'
               },
@@ -230,24 +192,60 @@ const expoMode = async(event) =>{
         }
       }
     })
-    event.reply(exponBubble);
-  }else{
+    }
+  // 輸入 'E + 關鍵字'---------------------------->  
+  if(userTypeStr === `E${userTypeNumStr}`){    
+    await axios
+    .get(`https://cons.judicial.gov.tw/jcc/zh-tw/jep03?interYear=&interNo=&interKeyword=${encodeURI(userTypeNumStr)}&startDate=&endDate=&submit=`)
+    .then(function (response) {
+      const $ = cheerio.load(response.data)
+      page = $('.pagination .PagedList-pageCountAndLocation').find('a').text()
+      $('body > div.container.blocky_container.footer_fix > div.container.blocky_container.footer_fix > div > table > tbody > tr > td:nth-child(2)').each(
+        function (index, element) {
+          titles.push($(element).text().trim())
+          titleLinks.push($(element).find('a').attr('href'))
+        }
+        )
+      })
+      // 搜尋結果不止一頁
+      if (page !== '') {
+        console.log(page)
+        totalPageNum = parseInt(/共(\d+)頁/g.exec(page)[1])
+        while (nowPage <= totalPageNum) {
+          await axios
+          .get(`https://cons.judicial.gov.tw/jcc/zh-tw/jep03?page=${nowPage}&interKeyword=${encodeURI(userTypeNumStr)}`)
+          .then(function (response) {
+            const $ = cheerio.load(response.data)
+            console.log(nowPage)
+            $(
+              'body > div.container.blocky_container.footer_fix > div.container.blocky_container.footer_fix > div > table > tbody > tr > td:nth-child(2)'
+              ).each(function (index, element) {
+                titles.push($(element).text().trim())
+                titleLinks.push($(element).find('a').attr('href'))
+              })
+              nowPage++
+          })
+        }
+      }
+    //整裡字串 
+    for (let i = 0; i < titles.length; i++) {
+      replyStr += `⚆ ${titles[i]}\n`
+    }
     event.reply(`🔎 搜尋結果:\n${replyStr}`);
-  }
-  return replyStr; 
+  }else{
+    event.reply(exponBubble);
+  } 
 }       
 // E 字串切割
 function exponNum(event) {
-  let finalNum;
   let userTypeStr = event.message.text;
   let userTypeArr = userTypeStr.split('');
-  let userTypeNumStr = '';
   userTypeArr.splice(0, 1);
+  let userTypeNumStr = '';
   for (let i = 0; i < userTypeArr.length; i++) {
     userTypeNumStr += userTypeArr[i];
-    finalNum = parseInt(userTypeNumStr);
   }
-  return finalNum;
+  return userTypeNumStr;
 }
 
 // 法條查詢---------------------------------------------------->
@@ -456,34 +454,27 @@ bot.on('message', async event => {
     try {
       if(expoOpen){ //釋字查詢
         console.log('expoopen');
-        if(event.message.text === '阿國，最近有什麼新鮮事嗎？'){
-          expoOpen = false;
-          newsOpen = true;
-          event.reply(newsQuickReply);
-        }else if(event.message.text === '阿國，我想要查法條！'){ 
+        if(event.message.text === '阿國，我想要查法條！'){ 
           expoOpen = false;
           lawOpen = true;
+          xianfaOpen = false;
+          mingfaOpen = false;
+          shinfaOpen = false;
+          shinzenfaOpen = false;
           event.reply(lawQuickReply);
-        }else if(event.message.text === '阿國，跟我說說這裡的規矩吧！'){
-          expoOpen = false;
-          ruleOpen = true;
         }else{
           replyStr = expoMode(event);
         }
       }else if(lawOpen){ //法條查詢
         console.log('lawopen');
-        if(event.message.text === '阿國，最近有什麼新鮮事嗎？'){
-          lawOpen = false;
-          newsOpen = true;
-          event.reply(newsQuickReply);
-        }else if(event.message.text === '阿國，我想要查釋字！'){
+        if(event.message.text === '阿國，我想要查釋字！'){
           lawOpen = false;
           expoOpen = true;
-        }else if(event.message.text === '阿國，跟我說說這裡的規矩吧！'){
-          lawOpen = false;
-          ruleOpen = true;
-        }else if(event.message.text === '阿國，我想要查法條！'){
-          console.log('又按了一次法條查詢');          
+          xianfaOpen = false;
+          mingfaOpen = false;
+          shinfaOpen = false;
+          shinzenfaOpen = false;
+        }else if(event.message.text === '阿國，我想要查法條！'){          
           xianfaOpen = false;
           mingfaOpen = false;
           shinfaOpen = false;
@@ -494,46 +485,18 @@ bot.on('message', async event => {
         } 
       }else{  //按鈕開關狀態設定
         if(event.message.text === '阿國，我想要查釋字！'){
-          console.log('第一次按釋字查詢');
+          console.log('釋字查詢');
           expoOpen = true;
           lawOpen = false;
-          newsOpen = false;
-          ruleOpen = false;
-        }else if(event.message.text === '阿國，最近有什麼新鮮事嗎？'){
-          expoOpen = false;
-          lawOpen = false;
-          newsOpen = true;
-          ruleOpen = false;
-          newsQuickReply = {
-            "type": "text", 
-            "text": "來！想要看什麼自己選 σ`∀´)σ",
-            "quickReply": {
-              "items": [
-                {
-                  "type": "action",
-                  "action": {
-                    "type": "uri",
-                    "label": "🔨司法院大法官",
-                    "uri": "https://cons.judicial.gov.tw/jcc/zh-tw"
-                  }
-                },
-                {
-                  "type": "action",
-                  "action": {
-                    "type": "uri",
-                    "label": "📐全國法規資料庫",
-                    "uri": "https://law.moj.gov.tw/News/NewsList.aspx"
-                  }
-                }
-              ]
-            }
-          }
         }else if(event.message.text === '阿國，我想要查法條！'){
-          console.log('第一次按法條查詢');
+          console.log('法條查詢');
           expoOpen = false;
           lawOpen = true;
-          newsOpen = false;
-          ruleOpen = false;
+
+          xianfaOpen = false;
+          mingfaOpen = false;
+          shinfaOpen = false;
+          shinzenfaOpen = false;
           lawQuickReply = {
             "type": "text", 
             "text": "好勒！你想要查哪一部法典呢？",
@@ -587,13 +550,20 @@ bot.on('message', async event => {
           event.reply({
             type: 'text',
             text:
-              '哈囉~~我叫阿國，\n\n我的創造者之前也是位國考生，在幾次的登陸失敗後，她毅然決然地躍入另一個火坑（還好~~不然你們就看不到我了_(:3 」∠ )_)...\n\n她說不管你是已上岸的也好，或是快擱淺了(誤)，儘管我仍有點兩光兩光，但還是希望多少能幫助到你，也謝謝你願意給我這個表現機會！ԅ(¯﹃¯ԅ)\n\n另外，偷偷跟你說，輸入 "nasa" 會有小彩蛋喔！\n\n最後，送你我很喜歡的一句話：\n「我們都是唯一的，像星星一樣，我們都是最好的。」(林達陽．暗中發光)\n\nHave a nice day!(*´∀`)~♥'
+              '哈囉~~我是國釋無雙，不過這個名字太假掰，你們可以叫我阿國就好！(๑¯∀¯๑)\n\n我的創造者之前也是位國考生，在幾次的登陸失敗後，她毅然決然地躍入另一個火坑（還好~~不然你們就看不到我了_(:3 」∠ )_)...\n\n她說不管你是已上岸的也好，或是快擱淺了(誤)，儘管我仍有點兩光兩光，但還是希望多少能幫助到你，也謝謝你願意給我這個表現機會！ԅ(¯﹃¯ԅ)\n\n是說，原本我只需要查釋字就好，沒想到因為她覺得我是可造之材，所以又丟了一堆工作給我，說這樣才不會辜負「無雙」二字，不然就要給我改名只叫「國釋」了。･ﾟ･(つд`ﾟ)･ﾟ･\n\n教學期間，我眼看著底下的桌子越來越多裂紋，真是擔心哪天她的手就這麼拍到我身上來，現在思及此依舊心有餘悸(´_ゝ`)\n\n等等，有人在敲門，我去瞧瞧...'
+          })
+          break;
+        case '阿國，跟我說說這裡的規矩吧！':
+          event.reply({
+            type: 'text',
+            text: 
+              `好勒~仔細聽好囉！(ゝ∀･)b\n\n1️⃣ 先選擇模式\n    🔸釋字查詢\n    🔸最新資訊\n    🔸法條查詢\n2️⃣ 根據個別給出的指令進行操作\n    🔸釋字查詢\n        📎E + 關鍵字\n            ex.E比例原則\n        📎輸入字號\n            ex. 767\n    🔸法條查詢\n        📎K + 關鍵字\n            ex. K行為能力\n        📎輸入條號\n            ex. 13,15,75-79,756.7\n3️⃣ 欲切換模式再重複前兩項即可\n\n🐔小彩蛋：\n🔹輸入我的小名\n🔹輸入「自我介紹」`
           })
           break;
         case `阿國，我想要查釋字！`:
           event.reply({
             type: 'text',
-            text: `好勒！\n請稍等，我跟你講一下規則喔！\n\n📌關鍵字檢索：\n     1️⃣直接輸入關鍵字即可\n     2️⃣所有符合條件者會以清單方式列出\n\n📌單一釋字查詢：\n     1️⃣輸入"E + 釋字"(例：E804)\n     2️⃣僅會顯示該筆釋字的爭點、解釋文及其理由書連結。\n\n若查無資料， "🔎 搜尋結果:" 會顯示空白。\n另外，有時資料量較大，因為人手不足 (創造者剝削我(つд⊂))，所以我整理起來需要花點時間，屆時還請多多見諒呀！`
+            text: '好勒！\n請稍等，我跟你講一下規則喔！\n\n📌關鍵字檢索：\n     1️⃣"E + 關鍵字" (例：E同性)\n     2️⃣所有符合條件者會以清單方式列出\n\n📌單一釋字查詢：\n     1️⃣直接輸入字號 (例：748)\n     2️⃣僅會顯示該筆釋字的爭點、解釋文及其理由書連結。\n\n若查無資料， "🔎 搜尋結果:" 會顯示空白。    另外，有時資料量較大，因為人手不足 (創造者剝削我(つд⊂))，所以我整理起來需要花點時間，屆時還請多多見諒呀！'
           })
           break;
         case `阿國，最近有什麼新鮮事嗎？`:
@@ -611,19 +581,19 @@ bot.on('message', async event => {
         case '我想要查民法':
           event.reply({
             type: 'text',
-            text: '<民法>\n好der好der！\n以下是輸入規則！(〃∀〃)\n\n📌條文檢索：\n     輸入"K + 關鍵字" (例：K平等)\n\n📌條號查詢：\n    1️⃣單一條號\n        直接輸入數字 (例：5)\n    2️⃣多個條號\n        🔸不連續\n             以半型 " , " 區隔 (例：5,7)\n        🔸連續\n             以半型 " - " 區隔 (例：5-10)\n        🔸混用 (例：1,5-10,17)\n    3️⃣含有 "之" 的條號\n        以半型 " . " 區隔 (例：100.1)\n\n💡K 為大寫，且標點符號務必半型喔！'
+            text: '<民法>\n好der好der！\n以下是輸入規則！(〃∀〃)\n\n📌條文檢索：\n     輸入"K + 關鍵字" (例：K自然人)\n\n📌條號查詢：\n    1️⃣單一條號\n        直接輸入數字 (例：26)\n    2️⃣多個條號\n        🔸不連續\n             以半型 " , " 區隔 (例：6,26)\n        🔸連續\n             以半型 " - " 區隔 (例：6-9)\n        🔸混用 (例：6,26-30,44)\n    3️⃣含有 "之" 的條號\n        以半型 " . " 區隔 (例：709.2)\n\n💡K 為大寫，且標點符號務必半型喔！'
           })
           break;
         case '我想要查刑法':
           event.reply({
             type: 'text',
-            text: '<刑法>\n收到！\n不過先看一下這裡喔(ㅅ˘ㅂ˘)\n\n📌條文檢索：\n     輸入"K + 關鍵字" (例：K平等)\n\n📌條號查詢：\n    1️⃣單一條號\n        直接輸入數字 (例：5)\n    2️⃣多個條號\n        🔸不連續\n             以半型 " , " 區隔 (例：5,7)\n        🔸連續\n             以半型 " - " 區隔 (例：5-10)\n        🔸混用 (例：1,5-10,17)\n    3️⃣含有 "之" 的條號\n        以半型 " . " 區隔 (例：100.1)\n\n💡K 為大寫，且標點符號務必半型喔！'
+            text: '<刑法>\n收到！\n不過先看一下這裡喔(ㅅ˘ㅂ˘)\n\n📌條文檢索：\n     輸入"K + 關鍵字" (例：K沒收)\n\n📌條號查詢：\n    1️⃣單一條號\n        直接輸入數字 (例：2)\n    2️⃣多個條號\n        🔸不連續\n             以半型 " , " 區隔 (例：2,11)\n        🔸連續\n             以半型 " - " 區隔 (例：38-40)\n        🔸混用 (例：2,38-40)\n    3️⃣含有 "之" 的條號\n        以半型 " . " 區隔 (例：38.1)\n\n💡K 為大寫，且標點符號務必半型喔！'
           })
           break;
         case '我想要查行政程序法':
           event.reply({
             type: 'text',
-            text: '<行政程序法>\n歐給！\n老規矩如下蛤(o´罒`o)\n\n📌條文檢索：\n     輸入"K + 關鍵字" (例：K平等)\n\n📌條號查詢：\n    1️⃣單一條號\n        直接輸入數字 (例：5)\n    2️⃣多個條號\n        🔸不連續\n             以半型 " , " 區隔 (例：5,7)\n        🔸連續\n             以半型 " - " 區隔 (例：5-10)\n        🔸混用 (例：1,5-10,17)\n    3️⃣含有 "之" 的條號\n        以半型 " . " 區隔 (例：100.1)\n\n💡K 為大寫，且標點符號務必半型喔！\n\n另外，因 line 的回覆有字數限制，所以若搜尋結果的資料量太大，我可能會直接暈倒給你看( ×ω× )(我的創造者似乎也懶得解決這個問題)，此時我建議可以改由條號查詢的方式，將範圍縮小，我就會再滿血復活了！ᕦ(ò_óˇ)ᕤ'
+            text: '<行政程序法>\n歐給！\n老規矩如下蛤(o´罒`o)\n\n📌條文檢索：\n     輸入"K + 關鍵字" (例：K行政處分)\n\n📌條號查詢：\n    1️⃣單一條號\n        直接輸入數字 (例：2)\n    2️⃣多個條號\n        🔸不連續\n             以半型 " , " 區隔 (例：2,20)\n        🔸連續\n             以半型 " - " 區隔 (例：92-97)\n        🔸混用 (例：2,92-97)\n    3️⃣含有 "之" 的條號\n        以半型 " . " 區隔 (例：174.1)\n\n💡K 為大寫，且標點符號務必半型喔！\n\n另外，因 line 的回覆有字數限制，所以若搜尋結果的資料量太大，我可能會直接暈倒給你看( ×ω× )(我的創造者似乎也懶得解決這個問題)，此時我建議可以改由條號查詢的方式，將範圍縮小，我就會再滿血復活了！ᕦ(ò_óˇ)ᕤ'
           })
           break;
       }
